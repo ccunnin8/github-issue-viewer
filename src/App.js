e03ec8b854daf4b6f4faabe5becb438eab560526
Corey Cunningham <coreyjjc@me.com>
2020-10-14 14:16:30 -0400

    put switch in router because for some reason the index is not search?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Results from "./components/Results";
 import Issue from "./components/Issue";
@@ -7,10 +7,12 @@ import Search from "./components/Search"
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={Search} />
-      <Route exact path ="/results" component={Results} />
-      <Route exact path="/:id" component={Results} />
-      <Route exact path="/issue/:id" component={Issue} />
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path ="/results" component={Results} />
+        <Route exact path="/:id" component={Results} />
+        <Route exact path="/issue/:id" component={Issue} />
+      </Switch>
     </Router>
   );
 }
