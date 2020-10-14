f44df4c78e760d8996f66c301f782936f04b44fc
Corey Cunningham <coreyjjc@me.com>
2020-10-14 14:13:36 -0400

    Updates

---
+++ precache-manifest.31edd4893b84310beb7a071d2f9fc78b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7650a5086c71685bdfe598228614eb1",
+    "url": "/github-issue-viewer/index.html"
+  },
+  {
+    "revision": "b6964c12ed291805e58f",
+    "url": "/github-issue-viewer/static/css/main.f523e9f7.chunk.css"
+  },
+  {
+    "revision": "e50a237a771aaf765fe1",
+    "url": "/github-issue-viewer/static/js/2.6ffea125.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-issue-viewer/static/js/2.6ffea125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6964c12ed291805e58f",
+    "url": "/github-issue-viewer/static/js/main.2714d19d.chunk.js"
+  },
+  {
+    "revision": "d988af526d7cc0f8e423",
+    "url": "/github-issue-viewer/static/js/runtime-main.d1972709.js"
+  }
+]);
